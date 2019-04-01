b38d253a283202ce3f82a938697389e359ffeca6
Brandon Ruoff <brandoru15@gmail.com>
2019-04-01 00:14:05 -0500

    Created a start button that reveals the battle scene once pressed

--- sub-sites/pokemon-battle.js
+++ sub-sites/pokemon-battle.js
@@ -1,12 +1,12 @@
 var playerOne;
 var opponent;
-initialize();
+var playerArray;
 
 function Player(theName, theClassify) {
 	this.name = theName;
 	this.classify = theClassify;
 	this.pokeParty = [];
-	this.currentPoke = this.pokeParty[0];
+	this.currentPoke;
 }
 
 function Pokemon(theName) {
@@ -18,22 +18,21 @@ function Pokemon(theName) {
 	this.ability;
 }
 
-function useMove(player, moveNum) {
-	document.getElementById('text-area').innerHTML =  player.currentPoke.name + " used move" + moveNum + "!";
-	
+function useMove(playerNum, moveNum) {
+	document.getElementById('text-area').innerHTML = playerArray[playerNum].currentPoke.name + " used Move " + (moveNum+1) + "!";
 }
 
 function addPokeToParty(player, pokeName) {
 	player.pokeParty.push(new Pokemon(pokeName));
-	if(player.currentPoke == undefined) {
-		player.currentPoke = player.pokeParty[0].name;
+	if(player.pokeParty.length == 1) {
+		player.currentPoke = player.pokeParty[0];
 	}
 }
 
 function initialize() {
 	playerOne = new Player('Player', 'player');
-	document.getElementById('text-area').innerHTML = playerOne.name;
 	opponent = new Player('Brandon', 'opponent');
+	playerArray = [playerOne, opponent];
 	addPokeToParty(playerOne, "venusaur");
 	addPokeToParty(playerOne, "charizard");
 	addPokeToParty(playerOne, "blastoise");
@@ -51,22 +50,15 @@ function initialize() {
 	document.getElementById('opponent-poke').src = "https://play.pokemonshowdown.com/sprites/xyani/" + opponent.currentPoke.classify + ".gif";
 	document.getElementById('player-status-bar').innerHTML = playerOne.currentPoke.name;
 	document.getElementById('opponent-status-bar').innerHTML = opponent.currentPoke.name;
-	switchPoke(playerOne, 1);
-	switchPoke(opponent, 1);
-	switchPoke(playerOne, 2);
-	switchPoke(opponent, 2);
-	switchPoke(playerOne, 3);
-	switchPoke(opponent, 3);
-	switchPoke(playerOne, 4);
-	switchPoke(opponent, 4);
-	switchPoke(playerOne, 5);
-	switchPoke(opponent, 5);
+	document.getElementById('battle-area').style.visibility = "visible";
+	document.getElementById('text-area').style.visibility = "visible";
+	document.getElementById('button-row').style.visibility = "visible";
+	document.getElementById('start-button').style.visibility = "hidden";
 }
 
 function switchPoke(player, pokeNum) {
 	playerName = player.name;
 	player.currentPoke = player.pokeParty[pokeNum];
-	//document.getElementById('text-area').innerHTML = 'CurrentPoke changed...';
 	document.getElementById(player.classify + '-status-bar').innerHTML = jsUcFirst(player.currentPoke.name);
 	if(player.classify == 'player') {
 		document.getElementById(player.classify + '-poke').src = "https://play.pokemonshowdown.com/sprites/xyani-back/" + player.currentPoke.classify + ".gif";
